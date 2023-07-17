c90416ab4676198595ff29e7193a1732546edc2d
Mayureshpawar29 <pawarmayuresh29@gmail.com>
2023-07-17 17:20:21 +0530

    removed unwanted line

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :saml_authenticatable,
+         :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: %i[azure_activedirectory_v2]
 end
